9c195f00a225392cc0c1b5f62368a5e1fd1e136f
Caleb <caldough@iu.edu>
2023-02-21 12:47:26 -0500

    Adding a counter for button presses

--- Milestone 1/Assets/Scripts/Randomization.cs
+++ Milestone 1/Assets/Scripts/Randomization.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Randomization : MonoBehaviour
 {
    public int yPos;
    public int xPos;
-   public GameObject theButton; 
-  
+   public GameObject theButton;
+   
+   Text pressesLeft;
+    public int buttonPresses;
+
     // Update is called once per frame 
+    private void Start()
+    {
+        buttonPresses = 15;
+        pressesLeft = GetComponent<Text>();
+        pressesLeft.text = "Presses Left: " + buttonPresses;
+    }
     void Update()
     {
       if(Input.GetButtonDown("Submit"))
@@ -16,7 +26,9 @@ public class Randomization : MonoBehaviour
            yPos = Random.Range( -425, 425);
            xPos = Random.Range( -874, 874);
            theButton.transform.localPosition = new Vector3(xPos,yPos, 0);
-            print(theButton.transform.localPosition);
+
+            buttonPresses = buttonPresses - 1;
+            pressesLeft.text = "Presses Left: " + buttonPresses;
         } 
     }
 }
