bf50ac2d35b17b2ece04aba6cf9058205915f892
jpay12 <97638950+jpay12@users.noreply.github.com>
2023-03-02 00:26:31 -0500

    Added Icons to the charcters
    Currently trying to get the icons to send the player to the correct minigame

--- Milestone 1/Assets/Scripts/SceneLoader.cs
+++ Milestone 1/Assets/Scripts/SceneLoader.cs
@@ -11,7 +11,7 @@ public class SceneLoader: MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
      }
 
-    public void LoadStartScene () 
+    public void LoadStartScene() 
     {
         SceneManager.LoadScene(0);
     } 
