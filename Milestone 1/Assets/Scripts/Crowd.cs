628b77398262f0147a99bcbabd19d5315a736c72
Corbin Maze <corbinmaze@gmail.com>
2023-03-30 18:18:34 -0400

    Created new ending Scene, Changed Dialogue, Tweaked some assets to better show time, changed Menu screen background to a more appropriate one

--- Milestone 1/Assets/Scripts/Crowd.cs
+++ Milestone 1/Assets/Scripts/Crowd.cs
@@ -31,45 +31,41 @@ public class Crowd : MonoBehaviour
     IEnumerator HideAndShow (float delay)
     {
 
-        Owl.SetActive(false);
-        yield return new WaitForSeconds(1f);
-        Owl.SetActive(true);
-
-        Fish.SetActive(false);
-        yield return new WaitForSeconds(4f);
-        Fish.SetActive(true);
-
         PossumEmotion1.SetActive(false);
-        yield return new WaitForSeconds(5f);
-        PossumEmotion1.SetActive(true);
-
-        PossumEmotion2.SetActive(false);
-        yield return new WaitForSeconds(7f);
-        PossumEmotion2.SetActive(true);
-
-        OwlEmotion.SetActive(false);
-        yield return new WaitForSeconds(1f);
-        OwlEmotion.SetActive(true);
-
-        OwlEmotion1.SetActive(false);
-        yield return new WaitForSeconds(7f);
-        OwlEmotion1.SetActive(true);
-
-        OwlEmotion2.SetActive(false);
-        yield return new WaitForSeconds(9f);
-        OwlEmotion2.SetActive(true);
-
-        FishEmotion.SetActive(false);
+        PossumEmotion.SetActive(true);
         yield return new WaitForSeconds(4f);
+        PossumEmotion1.SetActive(true);
+        PossumEmotion.SetActive(false);
+
+        OwlEmotion.SetActive(true);
+        OwlEmotion1.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        OwlEmotion1.SetActive(true);
+        OwlEmotion.SetActive(false);
+
         FishEmotion.SetActive(true);
-
         FishEmotion1.SetActive(false);
-        yield return new WaitForSeconds(9f);
+        yield return new WaitForSeconds(3f);
         FishEmotion1.SetActive(true);
+        FishEmotion.SetActive(false);
 
+        PossumEmotion1.SetActive(true);
+        PossumEmotion2.SetActive(false);
+        yield return new WaitForSeconds(4f);
+        PossumEmotion2.SetActive(true);
+        PossumEmotion1.SetActive(false);
+
+        OwlEmotion1.SetActive(true);
+        OwlEmotion2.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        OwlEmotion2.SetActive(true);
+        OwlEmotion1.SetActive(false);
+
+        FishEmotion1.SetActive(true);
         FishEmotion2.SetActive(false);
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(3f);
         FishEmotion2.SetActive(true);
+        FishEmotion1.SetActive(false);
     } 
 
 }
