87fa828adfb7fc3ed854233e716f067a6b449ee0
LAP118 <89601291+00Corby@users.noreply.github.com>
2023-03-30 15:59:22 -0400

    Changed Timers for spawns

--- Milestone 1/Assets/Scripts/Crowd.cs
+++ Milestone 1/Assets/Scripts/Crowd.cs
@@ -30,18 +30,46 @@ public class Crowd : MonoBehaviour
 
     IEnumerator HideAndShow (float delay)
     {
+
         Owl.SetActive(false);
         yield return new WaitForSeconds(1f);
-        Owl.SetActive(true); 
+        Owl.SetActive(true);
 
         Fish.SetActive(false);
         yield return new WaitForSeconds(4f);
-        Fish.SetActive(true); 
-          
-        PossumEmotion.SetActive(true);
-        yield return new WaitForSeconds(2f);
-        PossumEmotion.SetActive(false); 
-     
+        Fish.SetActive(true);
+
+        PossumEmotion1.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        PossumEmotion1.SetActive(true);
+
+        PossumEmotion2.SetActive(false);
+        yield return new WaitForSeconds(7f);
+        PossumEmotion2.SetActive(true);
+
+        OwlEmotion.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        OwlEmotion.SetActive(true);
+
+        OwlEmotion1.SetActive(false);
+        yield return new WaitForSeconds(7f);
+        OwlEmotion1.SetActive(true);
+
+        OwlEmotion2.SetActive(false);
+        yield return new WaitForSeconds(9f);
+        OwlEmotion2.SetActive(true);
+
+        FishEmotion.SetActive(false);
+        yield return new WaitForSeconds(4f);
+        FishEmotion.SetActive(true);
+
+        FishEmotion1.SetActive(false);
+        yield return new WaitForSeconds(9f);
+        FishEmotion1.SetActive(true);
+
+        FishEmotion2.SetActive(false);
+        yield return new WaitForSeconds(10f);
+        FishEmotion2.SetActive(true);
     } 
 
 }
