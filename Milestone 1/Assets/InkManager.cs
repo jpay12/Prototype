51ef7731d65398e5088023364a39b65cbea9ca5c
Caleb <caldough@iu.edu>
2023-04-20 22:02:39 -0400

    Made Ink Manager object and script to control Ink

---
+++ Milestone 1/Assets/InkManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+
+public class InkManager : MonoBehaviour
+{
+
+    public TextAsset _inkJsonAsset;
+    public Story _story;
+    public Text _textField;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
